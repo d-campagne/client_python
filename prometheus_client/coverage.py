ca968beeec4a6a5a646b85b012e64e92a8b17c2a
d-campagne <121505924+d-campagne@users.noreply.github.com>
2024-07-10 04:51:25 +0200

    Add files via upload
    Signed-off-by: d-campagne <121505924+d-campagne@users.noreply.github.com>

---
+++ prometheus_client/coverage.py
@@ -0,0 +1,9 @@
+branch_coverage_1 = {
+    "_bake_output_1": False,  
+    "_bake_output_2": False,
+}
+
+branch_coverage_2 = {
+    "write_to_textfile_1": False,
+    "write_to_textfile_1":False
+}
